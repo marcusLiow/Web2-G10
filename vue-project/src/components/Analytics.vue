acfe54781eb017f976632f86e6bf95cd4d28d9f0
Cong <34977007+congelat@users.noreply.github.com>
2025-10-11 02:51:59 +0800

    idk what im doing

---
+++ vue-project/src/components/Analytics.vue
@@ -0,0 +1,42 @@
+<script>
+import Chart from 'chart.js/auto';
+    export default { 
+        data() {
+            return {
+                msg : "Hello World"
+            }
+        },
+        mounted(){    
+            const ctx = document.getElementById('myChart');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                labels: ['Jun', 'Jul', 'Aug', 'Sept', 'October'],
+                datasets: [{
+                    label: '$ Spent in app',
+                    data: [120, 190, 30, 50, 20],
+                    borderWidth: 1
+                }]
+                },
+                options: {
+                scales: {
+                    y: {
+                    beginAtZero: true
+                    }
+                }
+                }
+            });
+        }
+    }
+</script>
+
+
+<template>
+    <canvas id="myChart"></canvas>
+</template>
+
+<style scoped>
+.greeting {
+    color: red;
+}
+</style>
---
+++ vue-project/src/components/MainMenu.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<script>
+    export default { 
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<template>   
+
+    <RouterLink to="/Analytics/">Analytics</RouterLink>
+    <RouterLink to="/AnalyticsDashboard/">Analytics Dashboard</RouterLink>
+   
+</template>
+
+<style scope>
+    a {
+        margin: 5px;
+        display: block;
+    }
+</style>
