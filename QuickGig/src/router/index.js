75369693cc8a38e907bbebcc867a829c0f12e327
Angel Lim Ann Qi <angellim@Angels-MacBook-Pro.local>
2025-10-14 16:03:09 +0800

    added job page for reference

--- QuickGig/src/router/index.js
+++ QuickGig/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import ProfilePage from '@/pages/ProfilePage.vue'
+import JobBoard from '@/pages/JobBoard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/profile',
       name: 'profile',
       component: ProfilePage
+    },
+    {
+      path: '/job',
+      name: 'job',
+      component: JobBoard
     }
   ]
 })
